fed7373b3ea1b8f28c0768d7cd621a83242c1235
Quantum-Coder826 <b.veldthuis@gmail.com>
2026-05-26 14:47:48 +0200

    stash

--- potatoes.R
+++ potatoes.R
@@ -1,5 +1,6 @@
 library(GWASpoly)
-library(ggplot2)
+library(tidyverse)
+source("./qq.R")
 
 # Ik zet mijn wd hadmatig
 #setwd("C:/Users/Hp/OneDrive - NHL Stenden/minor data science/gwasie")
@@ -34,21 +35,32 @@ if (!exists("data.loco.scan")) {
   data.loco.scan <- readRDS("./outputs/data_loco_scan.rds")
 }
 
-qq.plot(data.loco.scan,trait="RIJPTIJD") + ggtitle(label="Rijptijd")
+qq.plot(data.loco.scan, trait = "KOOKSCORE")
+
+qq(data.loco.scan, trait="RIJPTIJD") %>%
+  filter(Chrom == c("chr03", "chr04", "chr05")) %>%
+    ggplot(aes(x, y, colour = model)) + 
+    facet_wrap(~Chrom) + 
+    geom_point() + 
+    theme_bw() + 
+    xlab(expression(paste("Expected -log"[10], "(p)", sep = ""))) + 
+    ylab(expression(paste("Observed -log"[10], "(p)", sep = ""))) + 
+    scale_colour_brewer(palette = "Set1") + 
+    geom_abline(slope = 1, intercept = 0, linetype = 2) + 
+    theme(text = element_text(size = 15))
 
 #m.eff gaf niks, te streng?
-data2 <- set.threshold(data.loco.scan,method="Bonferroni",level=0.05,n.core=NCORE)
-#data2 <- set.threshold(data.loco.scan,method="M.eff",level=0.05) # errors???
+data2 <- set.threshold(data.loco.scan,method="M.eff",level=0.05) # errors???
 
 p <- manhattan.plot(data2,traits="RIJPTIJD")
 p + theme(axis.text.x = element_text(angle=90,vjust=0.5))
 manhattan.plot(data2,traits="RIJPTIJD",chrom="chr03") #NOTE: chromesomen zijn chrXX geformateerd
 
 #wont work
-p <- LD.plot(data, max.loci=1000)
+p <- LD.plot(data2, max.loci=1000)
 p + xlim(0,30) 
 
-qtl <- get.QTL(data=data2,traits="KOOKTYPE_num",models="additive",bp.window=5e6)
+qtl <- get.QTL(data=data2,traits="KOOKTYPE",models="additive",bp.window=5e6)
 knitr::kable(qtl)
 
 fit.ans <- fit.QTL(data=data2,trait="KOOKTYPE_num",
