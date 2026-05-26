1909f8e03f3ad42db31793f7c64a52d0567dc737
Quantum-Coder826 <b.veldthuis@gmail.com>
2026-05-26 13:20:55 +0200

    Made qq() out of qq.Plot() to allow working with ggplot()

---
+++ qq.R
@@ -0,0 +1,34 @@
+qq <- function(data, trait, model = NULL){
+  stopifnot(inherits(data, "GWASpoly.fitted"))
+  stopifnot(is.element(trait, names(data@scores)))
+  all.models <- colnames(data@scores[[trait]])
+  if (is.null(models)) {
+    models <- all.models
+  } else {
+    dom.models <- models[grep("dom", models, fixed = T)]
+    models <- setdiff(models, dom.models)
+    if (length(dom.models) > 0) {
+      dom.models <- unlist(lapply(as.list(dom.models), 
+                                  function(x) {
+                                    all.models[grep(x, all.models, fixed = T)]
+                                  }))
+    }
+    models <- union(models, dom.models)
+    stopifnot(all(is.element(models, all.models)))
+  }
+  scores <- as.data.frame(data@scores[[trait]][, models])
+  colnames(scores) <- models
+  scores$Chrom <- data@map$Chrom
+  tmp <- pivot_longer(data = scores, cols = 1:length(models), 
+                      names_to = "model", values_to = "y", values_drop_na = TRUE)
+  tmp <- as.data.frame(tmp)
+  tmp$model <- factor(tmp$model, levels = models, ordered = T)
+  tmp <- tmp[order(tmp$model, tmp$Chrom, tmp$y, decreasing = c(FALSE, 
+                                                               FALSE, TRUE)), ]
+  tmp2 <- tapply(tmp$y, list(tmp$Chrom, tmp$model), function(x) {
+    n <- length(x)
+    unif.p <- -log10(ppoints(n))
+  })
+  tmp$x <- unlist(tmp2)
+  return(tmp) 
+}
