a6e697e1f885e958b7191a8f59f2f0ec4b74cab9
Quantum-Coder826 <b.veldthuis@gmail.com>
2026-06-01 15:42:58 +0200

    Basis voor ciclize

--- circos.R
+++ circos.R
@@ -10,23 +10,49 @@ chrom_lenghts <- read_csv("data/potato_chrom_lengths_DMv6.csv")
 ###################################
 qtl.table$coord <- paste0(qtl.table$Chrom, ":", qtl.table$Position, "..", qtl.table$Position)
 
-
 circos.genomicInitialize(chrom_lenghts)
+
+model.shape <- c(
+  "additive" = 15,
+  "1-dom-alt" = 16,
+  "1-dom-ref" = 17 
+)
+
+trait.colors <- c(
+  "KOOKSCORE" = "red",
+  "NABAKKEN" = "orange",
+  "RIJPTIJD" = "green",
+  "VERKLEURING.KOKEN" = "blue",
+  "VLEESKLEUR.NA.KOKEN" = "purple"
+)
 
 circos.trackPoints(
   factors = qtl.table$Chrom,
   x = qtl.table$Position,
   y = rep(0, nrow(qtl.table)),
-  pch = 3, cex = 1.2
+  cex = 1.2,
+  pch = model.shape[qtl.table$Model],
+  col = trait.colors[qtl.table$Trait]
 )
+
 
 circos.trackText(
   factors = qtl.table$Chrom,
   x = qtl.table$Position,
-  y = rep(-1, nrow(qtl.table)),
+  y = rep(-1.5, nrow(qtl.table)),
   labels = qtl.table$Marker,
   facing = "clockwise",
   cex = 0.7
 )
 
+legend.traits <- Legend(at = c("KOOKSCORE","NABAKKEN","RIJPTIJD","VERKLEURING.KOKEN","VLEESKLEUR.NA.KOKEN"),
+       title = "Trait", legend_gp = gpar(fill=c("red", "orange", "green", "blue", "purple")))
+
+legend.models <- Legend(at = c("additive", "1-dom-alt", "1-dom-ref"),
+                        type = "points", pch = 15:17)
+
+legends <- packLegend(legend.models, legend.traits)
+draw(legends, x = unit(5, "mm"), y = unit(10, "mm"), just = c("left", "bottom"))
+
 circos.clear()
+
